158a87c693e8b493cb5017b461eec64a690b8290
djneo4 <nsiga@live.com.ar>
2016-08-16 20:24:20 -0300

    Esto es una prueba

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>	
+
+<head>
+		<meta charset="UTF-8">
+		<title>Siga</title>
+		
+</head>
+
+<body>
+			<h1>Hola mundo</h1>
+
+			<?php 
+						$numero = 10;
+
+						echo "El numero es: $numero";
+
+						echo "<h2>Hola mundo</h2>";
+
+			?>
+
+
+</body>
+
+
+</html>
+
+
